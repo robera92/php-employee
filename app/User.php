4b921da9cac976d86c4a9cdb4bcd5b37ede94c70
robera92 <robertas.arla@stud.kitm.lt>
2024-03-07 11:27:23 +0200

    php

--- app/User.php
+++ app/User.php
@@ -6,6 +6,26 @@ abstract class User{
     protected $userName;
     protected $email;
 
+    public function setUserName(string $userName)
+    {
+        $this->userName = $userName;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
     abstract public function showUserInfo();
 
 }
